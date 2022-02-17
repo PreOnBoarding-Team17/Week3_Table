1c15be76755a2084b11980dd1147e2c000384d80
developjik <developjik@gmail.com>
2022-02-17 21:54:23 +0900

    feat: 프로젝트 초기 셋팅

---
+++ src/API/index.tsx
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:4000/data'
+
+export const fetchData = async () => {
+  const response = await axios.get(BASE_URL)
+  return response
+}
