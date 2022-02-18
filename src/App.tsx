14297aed166a2c3ca3fdf7ecc06238755f299e2e
som-syom <dhyeon@student.42seoul.kr>
2022-02-18 14:59:00 +0900

    feat #4: data grid 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchData } from 'API';
 import { Button } from '@mui/material';
+import Table from 'Components/Table';
 
 function App() {
+  const [allData, setAllData] = useState<any>([]);
   useEffect(() => {
     fetchData().then((res) => {
       console.log(res.data);
+      setAllData(res.data);
     });
   }, []);
 
@@ -15,6 +18,7 @@ function App() {
       <Button variant="contained" color="primary">
         Text
       </Button>
+      <Table datas={allData} />
     </div>
   );
 }
